d92ec7185db9fddc301d19beac2021c6712e338d
Amber Aly <amberaly123@gmail.com>
2024-09-24 14:27:57 +0500

    add files

---
+++ hello-world/src/app/page.tsx
@@ -0,0 +1,12 @@
+
+
+export default function Home() {
+  return (
+<div>
+  <h1 className="text-red-500 bg-purple-500  h-screen text-center flex-justify-center  
+  items-center text-6xl">
+    HELLO WORLD!</h1>
+</div>
+
+  );
+}
